c37dc16146264083b77c6317c17d5b8688dabf9d
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-12-14 10:46:08 +0100

    voeg database table toe

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template, Request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/sign_up", methods=["GET","POST"])
+def sign_up():
+    if request.method == "POST":
+        naam = request.form.get("naam")
+        wachtwoord = request.form.get("wachtwoord")
+        begin_cash = request.form.get("begin_cash")
+        print(naam)
+        print(wachtwoord)
+        print(begin_cash)
+    return render_template("sign_up.html")
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0")
--- database_creation.py
+++ database_creation.py
@@ -1,1 +1,7 @@
-from sqlite3 import Cursor, Connection
+from sqlite3 import Cursor, Connection, connect
+
+con :Connection = connect("database.db")
+cur :Cursor = con.cursor()
+
+cur.execute("CREATE TABLE IF NOT EXISTS accounts(naam TEXT PRIMARY KEY, wachtwoord TEXT, balance INTEGER)")
+con.commit()
