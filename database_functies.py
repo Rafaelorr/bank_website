509336f4a74f8fe58bb60981ba1885daa6acf589
Rafaelorr <rafael.onkelinx@muscardinus.be>
2024-12-19 17:54:40 +0100

    verwijder niet gebruikte functies in database_functies.py

--- database_functies.py
+++ database_functies.py
@@ -1,10 +1,5 @@
 from sqlite3 import Cursor, Connection, connect
 
-def basic_connection(database_naam:str):
-    con :Connection = connect(database_naam)
-    cur :Cursor = con.cursor()
-    return con, cur
-
 def sign_up_account(naam:str, wachtwoord:str, begin_cash:int):
   con :Connection = connect("database.db")
   cur :Cursor = con.cursor()  
@@ -14,9 +9,6 @@ def sign_up_account(naam:str, wachtwoord:str, begin_cash:int):
   con.commit()
   cur.close()
   con.close()
-
-#SELECT * FROM gebruikers WHERE naam=? AND wachtwoord=?",(naam,wachtwoord)
-#session["naam"]
 
 def delete_account(naam:str, wachtwoord:str):
     con : Connection = connect("database.db")
@@ -55,8 +47,3 @@ def login_account(naam:str, wachtwoord:str) -> str:
 
 def transaction_account(ontvanger:str, hoeveelheid:int):
     pass
-
-def sla_verandering_op(con: Connection,cur: Cursor):
-    con.commit()
-    cur.close()
-    con.close()
