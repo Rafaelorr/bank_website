0dd3b8f3ab73f0409e8af4c91b097e03324dfcfd
Rafaelorr <rafael.onkelinx@muscardinus.be>
2025-06-26 16:22:25 +0200

    voeg een id kolom toe aan de database

--- database_creation.py
+++ database_creation.py
@@ -3,5 +3,7 @@ from sqlite3 import Cursor, Connection, connect
 con :Connection = connect("database.db")
 cur :Cursor = con.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS accounts(naam TEXT PRIMARY KEY, wachtwoord TEXT, balance INTEGER)")
+cur.execute("DROP TABLE IF EXISTS accounts")
+
+cur.execute("CREATE TABLE IF NOT EXISTS accounts(id INTEGER PRIMARY KEY,naam TEXT KEY, wachtwoord TEXT, balance INTEGER)")
 con.commit()
