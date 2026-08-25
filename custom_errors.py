7d5c0c728563addd861aef035bd5a3318e2179cc
Rafaelorr <rafael.onkelinx@muscardinus.be>
2026-08-25 13:22:25 +0200

    past de linter regels toe op de custom_errors.py

--- custom_errors.py
+++ custom_errors.py
@@ -1,13 +1,18 @@
-class account_not_found(Exception):
+"""Dit zijn error messages, speciaal voor dit programma."""
+class AccountNotFound(Exception):
+    """Deze error triggert wanneer een account niet gevonden wordt in een querry."""
     def __init__(self):
         super().__init__("No matching account found.")
+        self.message = "No matching account found."
 
     def __str__(self):
         return f"{self.message}"
 
-class not_enough_funds(Exception):
+class NotEnoughFunds(Exception):
+    """Deze error triggert wanneer een gebruiker niet genoeg geld heeft voor een transactie."""
     def __init__(self):
         super().__init__("Sender doesn't have enough funds.")
+        self.message = "Sender doesn't have enough funds."
 
     def __str__(self):
         return f"{self.message}"
